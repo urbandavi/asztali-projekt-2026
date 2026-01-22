f87832ccb955041d78b1e61c2b58c0346b25e8fb
urbandavi <urbandavidpeter@turr.hu>
2026-01-22 11:33:15 +0100

    project-structure

---
+++ asztali-projekt-2026/Model/Class-Object.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asztali_projekt_2026.Model
+{
+    internal class Class_Object
+    {
+    }
+}
