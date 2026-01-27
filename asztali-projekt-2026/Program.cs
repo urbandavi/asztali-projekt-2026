21872633d12636aebdccf7fa36935f03e68ca170
LakatosBotond <lakatosbotond@turr.hu>
2026-01-27 09:14:11 +0100

    Connection string main

--- asztali-projekt-2026/Program.cs
+++ asztali-projekt-2026/Program.cs
@@ -1,7 +1,10 @@
-﻿internal class Program
+﻿using asztali_projekt_2026.Model;
+
+internal class Program
 {
+    public static readonly string connectionString = "Server=localhost;Database=foldrajz;User=root;";
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
     }
 }
