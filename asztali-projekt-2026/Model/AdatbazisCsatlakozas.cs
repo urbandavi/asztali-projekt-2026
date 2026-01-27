24e0553cd86ec82756a27073fca027da441fbd46
LakatosBotond <lakatosbotond@turr.hu>
2026-01-27 09:09:29 +0100

    adatbazis csatlakozás class

---
+++ asztali-projekt-2026/Model/AdatbazisCsatlakozas.cs
@@ -0,0 +1,36 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asztali_projekt_2026.Model
+{
+    internal class AdatbazisCsatlakozas
+    {
+
+        private static string connectionString;
+        private static string table;
+        private static string query_parameters;
+
+        public static void DbConnectionCheck(string connectionString)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection successful.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("SIkertelen kapcs");
+                Console.WriteLine(ex);
+
+            }
+
+        }
+    }
+}
