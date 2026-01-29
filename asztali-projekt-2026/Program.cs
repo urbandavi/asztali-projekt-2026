e9a84f30ec0e1c36aef0ea53b40440ebcb71e3ae
urbandavi <urbandavidpeter@turr.hu>
2026-01-29 11:13:13 +0100

    litle-chages-in-destribution

--- asztali-projekt-2026/Program.cs
+++ asztali-projekt-2026/Program.cs
@@ -1,7 +1,8 @@
 ﻿using asztali_projekt_2026.Model;
 
 internal class Program
-{
+{   
+    
     public static readonly string connectionString = "Server=localhost;Database=asztalifocik;User=root;";
     private static void Main(string[] args)
     {
