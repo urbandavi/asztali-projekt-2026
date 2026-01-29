e1dc2e70fac7aaeaff77d310ddc27d1a319ada4d
LakatosBotond <lakatosbotond@turr.hu>
2026-01-29 11:10:17 +0100

    adatbazis csatlakozás GetAllData és DeleteById

--- asztali-projekt-2026/Model/AdatbazisCsatlakozas.cs
+++ asztali-projekt-2026/Model/AdatbazisCsatlakozas.cs
@@ -1,6 +1,7 @@
 ﻿using MySqlConnector;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +31,36 @@ namespace asztali_projekt_2026.Model
                 Console.WriteLine(ex);
 
             }
+
+        }
+
+        public static DataTable GetAllData(string tableName, string connectionString)
+        {
+            using var connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            using var command = new MySqlCommand("select * from " + tableName, connection);
+
+            using var reader = command.ExecuteReader();
+            var dataTable = new DataTable();
+
+            dataTable.Load(reader);
+
+            return dataTable;
+
+        }
+
+
+        public static int DeleteById(string connectionString, string tableName, int id)
+        {
+            using var connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            using var command = new MySqlCommand($"delete from {tableName} where id=@id", connection);
+            command.Parameters.AddWithValue("@id", id);
+
+            return command.ExecuteNonQuery();
+            //1 = done, 0 = id nem létezik
 
         }
     }
